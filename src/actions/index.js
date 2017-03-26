6c6f6881788674b5aea73b9fffdf2a8d5d421b36
John Riepe <johnriepe@gmail.com>
2017-03-26 18:03:26 -0400

    added https to request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=jdr1964';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
